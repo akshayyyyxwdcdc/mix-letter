365a535f0449567b7d2c32e29fce077371bfc0ce
akshayyyyxwdcdc <163457518+akshayyyyxwdcdc@users.noreply.github.com>
2024-03-23 01:59:49 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// your-script.js
+// JavaScript code to handle button clicks
+
+// Function to navigate to Page 2
+function index1() {
+    window.location.href = "index1.html";
+}
+
+// Function to go back to the Home Page
+
+
+// Add event listeners to buttons
+document.getElementById('index1.html').addEventListener('click', index1);
+
